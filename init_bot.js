14bb57c2fdf585e4215eb107cea72a3824a8e1c3
JJ173 <jack.t371@hotmail.co.uk>
2018-01-11 00:00:53 +0000

    Added new QAI files
    Bot is being re-written by Rackover and JJ. This is the start of the new project.

---
+++ init_bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require("fs");
+
+//Config Files
+const config = require('./json/config.json');
+
+client.on('ready', () => {
+	console.log("I am ready!");
+	client.user.setGame(`with the Seven Hand Node.`);
+});
+
+client.login(config.token);
