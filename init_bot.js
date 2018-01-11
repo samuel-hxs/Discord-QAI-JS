37db7c82fb7b7914cb0bfc0a8c91547130496263
rackover <rackover@racknet.noip.me>
2018-01-11 11:42:42 +0100

    Added log function and function file - Untested

--- init_bot.js
+++ init_bot.js
@@ -1,17 +1,19 @@
+//LIBS
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
 
-//Config Files
-const config = require('./json/config.json');
+//UTILS
+const utils = require('./utils/funcs.js');
+
+//CONFIG
+const config = require('./sys/config.json');
+
+utils.log('QAIx fired ! Preparing...');
 
 client.on('ready', () => {
-	console.log("I am ready!");
+	utils.log('QAIx ready !');
 	client.user.setGame(`with the Seven Hand Node.`);
 });
-
-function log(message){
-	console.log(message);
-}
 
 client.login(config.token);
