9eba757bc327985433ac471c3474d16e45ecad57
rackover <rackover@racknet.noip.me>
2018-01-26 10:26:44 +0100

    Added SQLITE3 link

--- init_bot.js
+++ init_bot.js
@@ -2,6 +2,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
+const sqlite3 = require('sqlite3').verbose();
 
 //UTILS
 const utils = require('./utils/funcs.js');
